d77a8197f443629acca77c6fff970c158d588459
elenasmirnovaesm2-boop <elena.smirnova.esm2@gmail.com>
2025-12-09 01:12:46 +0100

    Create auth_config.py

---
+++ auth_config.py
@@ -0,0 +1,9 @@
+# auth_config.py
+
+# сюда ВПИШИ свой Telegram user_id вместо 123456789
+ALLOWED_USERS = {123456789}
+
+
+def is_allowed(user_id: int) -> bool:
+    """Проверяем, есть ли user_id в белом списке."""
+    return user_id in ALLOWED_USERS
